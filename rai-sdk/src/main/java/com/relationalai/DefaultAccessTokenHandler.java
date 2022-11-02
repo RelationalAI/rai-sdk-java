d2a163144e30813137da5ae3829bd7cfb34f9a02
helmi.nour <helmiinour@gmail.com>
2022-11-02 14:39:55 +0100

    enable token caching for integration tests

--- rai-sdk/src/main/java/com/relationalai/DefaultAccessTokenHandler.java
+++ rai-sdk/src/main/java/com/relationalai/DefaultAccessTokenHandler.java
@@ -39,8 +39,10 @@ public class DefaultAccessTokenHandler implements AccessTokenHandler {
     public AccessToken getAccessToken(Client client, ClientCredentials credentials)
             throws HttpError, InterruptedException, IOException {
         var token = readAccessToken(credentials);
-        if (token != null && !token.isExpired())
+        if (token != null && !token.isExpired()) {
+            System.out.println("===> using cached access token");
             return token;
+        }
 
         token = client.fetchAccessToken(credentials);
         if (token != null)
--- rai-sdk/src/test/java/com/relationalai/UserTest.java
+++ rai-sdk/src/test/java/com/relationalai/UserTest.java
@@ -30,7 +30,7 @@ import org.junit.jupiter.api.TestInstance.Lifecycle;
 
 // Test User APIs.
 // TODO: Keep it disabled until the Auth0 API rate limiting issue is fixed.
-@Disabled
+//@Disabled
 @TestInstance(Lifecycle.PER_CLASS)
 public class UserTest extends UnitTest {
     static UUID uuid = UUID.randomUUID();
